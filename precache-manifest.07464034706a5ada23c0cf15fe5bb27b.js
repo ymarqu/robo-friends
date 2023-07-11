22d3967ca01c974b06ca494666df29fd24e18dd5
ymarquina <yeseniamrqna@gmail.com>
2023-07-11 15:38:18 -0700

    Updates

---
+++ precache-manifest.07464034706a5ada23c0cf15fe5bb27b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8551f682350da0fea8b0",
+    "url": "/robo-friends/static/css/main.99635223.chunk.css"
+  },
+  {
+    "revision": "8551f682350da0fea8b0",
+    "url": "/robo-friends/static/js/main.8551f682.chunk.js"
+  },
+  {
+    "revision": "7f1ec99ba77e68a1913f",
+    "url": "/robo-friends/static/js/1.7f1ec99b.chunk.js"
+  },
+  {
+    "revision": "617e3c50e67378d4af10",
+    "url": "/robo-friends/static/js/runtime~main.617e3c50.js"
+  },
+  {
+    "revision": "dcac76e0ea14074c9e53f8b01c0f79ee",
+    "url": "/robo-friends/static/media/Ripple.dcac76e0.svg"
+  },
+  {
+    "revision": "d399236319a17612f106e73fa038a55e",
+    "url": "/robo-friends/index.html"
+  }
+];
